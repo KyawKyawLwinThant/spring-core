71fc70d35c795019520db3179aa6cc693bf4f04c
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2023-02-28 10:15:35 +0630

    spring core base

---
+++ sample-di/src/main/java/com/demo/Main.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
